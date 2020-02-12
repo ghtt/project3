2cc57889a804f7e89fed54f4141f194699f09628
ghtt <krasnoyarov1990@gmail.com>
2020-02-12 15:55:59 +0300

    Models changes #1

--- orders/admin.py
+++ orders/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Food, Menu, Price
+from .models import Category, Price, Product, Topping
 
-admin.site.register(Food)
-admin.site.register(Menu)
+admin.site.register(Category)
 admin.site.register(Price)
+admin.site.register(Product)
+admin.site.register(Topping)
--- orders/models.py
+++ orders/models.py
@@ -4,25 +4,54 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Price(models.Model):
-    price_type = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.price_type}"
-
-
-class Food(models.Model):
-    food_type = models.CharField(max_length=64)
-
-    def __str__(self):
-        return f"{self.food_type}"
-
-
-class Menu(models.Model):
-    food_type = models.ForeignKey(to=Food, on_delete=models.CASCADE)
+class Category(models.Model):
     name = models.CharField(max_length=64)
-    price_type = models.ForeignKey(to=Price, on_delete=models.CASCADE)
-    price = models.FloatField()
 
     def __str__(self):
-        return f"{self.food_type} {self.name} - {self.price_type} {self.price}"
+        return self.name
+
+
+class Product(models.Model):
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    TOPPINGS_NUMBER = [
+        (ONE, "One"),
+        (TWO, "Two"),
+        (THREE, "Three"),
+        (ZERO, "Not available"),
+    ]
+    name = models.CharField(max_length=64)
+    number_of_toppings = models.IntegerField(choices=TOPPINGS_NUMBER, default=ZERO)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # price ? FK or value?
+
+    def __str__(self):
+        name = " ".join(
+            [
+                str(self.category),
+                self.name,
+                str(self.number_of_toppings),
+                "topping" if self.number_of_toppings == 1 else "toppings",
+            ]
+        )
+        return name
+
+
+class Price(models.Model):
+    SMALL = "S"
+    LARGE = "L"
+    PRICE_TYPE = [(SMALL, "Small"), (LARGE, "Large")]
+    name = models.CharField(max_length=1, choices=PRICE_TYPE, default=SMALL)
+    value = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} = {self.value}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
